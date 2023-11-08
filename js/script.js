675763864a6964a6652d4c94a944032b9812aa07
Ewa Loos <ewa.loos@gmail.com>
2023-11-08 22:39:52 +0100

    first js attempt

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 {
-  const init = () => {};
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `<li> ${task.content} </li>`;
+    }
+
+    document.querySelector("js-tasks").innerHTML = htmlString;
+  };
+
+  const init = () => {
+    render();
+  };
 
   init();
 }
