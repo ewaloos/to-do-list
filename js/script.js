7d756dda081b4a9a3997a01323d56e71afeefef2
Ewa Loos <ewa.loos@gmail.com>
2023-11-09 15:59:15 +0100

    09.11 js tasks rendering

--- js/script.js
+++ js/script.js
@@ -1,16 +1,47 @@
 {
+  const tasks = [
+    {
+      content: "nagrać",
+      done: true,
+    },
+    {
+      content: "zjeść",
+      done: false,
+    },
+  ];
+
   const render = () => {
     let htmlString = "";
 
     for (const task of tasks) {
-      htmlString += `<li> ${task.content} </li>`;
+      htmlString += `<li ${
+        task.done ? ' style="text.decoration=line-through"' : ""
+      }> ${task.content} </li>`;
     }
 
-    document.querySelector("js-tasks").innerHTML = htmlString;
+    document.querySelector(".js-tasks").innerHTML = htmlString;
   };
 
   const init = () => {
     render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const newTaskContent = document.querySelector(".js-newTask").value.trim();
+      console.log(newTaskContent);
+
+      if (newTaskContent === "") {
+        return;
+      }
+
+      tasks.push({
+        content: newTaskContent,
+      });
+      render();
+    });
   };
 
   init();
